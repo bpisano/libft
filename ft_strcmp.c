9c656f9f98b1046579ec5a4b27926d2757095966
Benjamin Pisano <benjamin.pisano@icloud.com>
2018-01-15 14:19:00 +0100

    protected ft_strcmp

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: bpisano <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2017/11/24 13:25:24 by bpisano      #+#   ##    ##    #+#       */
-/*   Updated: 2017/11/24 13:25:26 by bpisano     ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/01/15 14:18:39 by bpisano     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -17,6 +17,8 @@ int		ft_strcmp(const char *s1, const char *s2)
 {
 	int		i;
 
+	if (!s1 || !s2)
+		return (0);
 	i = 0;
 	while (s1[i] && s2[i] && (unsigned char)s1[i] == (unsigned char)s2[i])
 		i++;
