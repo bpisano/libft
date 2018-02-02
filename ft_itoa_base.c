b844ccfba294370b9d952075830e4d744eead1b9
Benjamin Pisano <benjamin.pisano@icloud.com>
2018-02-02 14:12:34 +0100

    updated ft_itoa_base.c

--- ft_itoa_base.c
+++ ft_itoa_base.c
@@ -6,29 +6,45 @@
 /*   By: bpisano <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2017/11/24 13:06:51 by bpisano      #+#   ##    ##    #+#       */
-/*   Updated: 2017/11/24 13:06:54 by bpisano     ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/02/02 14:11:59 by bpisano     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_itoa_base(int n, int base)
+static void		signed_number(char **nbr, int n, int base)
+{
+	char	*itoa;
+
+	itoa = ft_itoa_base(-n, base);
+	*nbr = ft_strjoin("-", itoa);
+	ft_strdel(&itoa);
+}
+
+char			*ft_itoa_base(int n, int base)
 {
 	char	*nbr;
 	char	*s_base;
+	char	*sub;
+	char	*itoa;
 
-	if (!(nbr = (char *)ft_memalloc(sizeof(char))))
-		return (NULL);
 	s_base = "0123456789ABCDEF";
 	if (n == -2147483648)
-		nbr = ft_strjoin("-", ft_strjoin(ft_itoa_base(-(n / base), base),
-										ft_itoa_base(8, base)));
+		nbr = ft_strdup("-2147483648");
+	else if (n < 0 && base != 10)
+		nbr = ft_itoa_base(-n, base);
 	else if (n < 0)
-		nbr = ft_strjoin("-", ft_itoa_base(-n, base));
+		signed_number(&nbr, n, base);
+	else if (n < base)
+		nbr = ft_strsub(s_base, n % base, 1);
 	else
-		nbr = n < base ? ft_strdup(ft_strsub(s_base, n % base, 1)) :
-						ft_strjoin(ft_itoa_base(n / base, base),
-						ft_strsub(s_base, n % base, 1));
+	{
+		sub = ft_strsub(s_base, n % base, 1);
+		itoa = ft_itoa_base(n / base, base);
+		nbr = ft_strjoin(itoa, sub);
+		ft_strdel(&sub);
+		ft_strdel(&itoa);
+	}
 	return (nbr);
 }
