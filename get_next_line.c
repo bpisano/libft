e1030dd9694c74c32a1f2883e784f7576ffb13bc
Benjamin Pisano <benjamin.pisano@icloud.com>
2018-03-13 20:32:10 +0100

    fixed get_next_line leaks

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: bpisano <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2017/12/01 11:52:58 by bpisano      #+#   ##    ##    #+#       */
-/*   Updated: 2018/03/13 20:05:52 by bpisano     ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/03/13 20:30:19 by bpisano     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -24,35 +24,35 @@ int		n_index(char *str)
 	return (-1);
 }
 
-int		copy_rest(char **line, char *rest, n)
+int		copy_rest(char **line, char *rest, int realloc, int sup)
 {
-	
+	int		n;
+
+	if ((n = n_index(rest)) == -1)
+		return (0);
+	*line = realloc == 0 ? ft_strnew(n) : ft_realloc(*line, n + sup);
+	ft_strncat(*line, rest, n);
+	ft_strcpy(rest, rest + n + 1);
+	return (1);
 }
 
 int		get_next_line(const int fd, char **line)
 {
-	static char 	rest[BUFF_SIZE + 1];
+	static char		rest[BUFF_SIZE + 1];
 	int				n;
 	int				ret;
 
-	(BUFF_SIZE < 1 || !(*line) || read(fd, NULL, 0) < 0) ? return (-1) : 0;
-	if ((n = n_index(rest)) > -1 && (*line = ft_strnew(n)))
-	{
-		ft_strncpy(*line, rest, n);
-		ft_strcpy(rest, rest + n + 1);
+	if (BUFF_SIZE < 1 || !line || read(fd, NULL, 0) < 0)
+		return (-1);
+	if (copy_rest(line, &rest[0], 0, 0) == 1)
 		return (1);
-	}
 	*line = ft_strnew(ft_strlen(rest));
 	ft_strcpy(*line, rest);
 	while ((ret = read(fd, rest, BUFF_SIZE)))
 	{
 		rest[ret] = '\0';
-		if ((n = n_index(rest)) > -1 && (*line = ft_realloc(*line, n + ret)))
-		{
-			ft_strncat(*line, rest, n);
-			ft_strcpy(rest, rest + n + 1);
+		if (copy_rest(line, &rest[0], 1, ret) == 1)
 			return (1);
-		}
 		*line = ft_realloc(*line, ft_strlen(*line) + ret);
 		ft_strcat(*line, rest);
 	}
