55e41c0129e991a8de7c57bcc31583e5319c4d29
Benjamin Pisano <benjamin.pisano@icloud.com>
2018-03-21 12:46:36 +0100

    fixes

--- ft_strcat.c
+++ ft_strcat.c
@@ -6,7 +6,7 @@
 /*   By: bpisano <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2017/11/24 13:24:46 by bpisano      #+#   ##    ##    #+#       */
-/*   Updated: 2018/03/14 16:26:19 by bpisano     ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/03/21 12:46:23 by bpisano     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -18,7 +18,8 @@ char	*ft_strcat(char *s1, const char *s2)
 	int		i;
 
 	i = -1;
-	while (s1[++i]);
+	while (s1[++i])
+		;
 	ft_strcpy(s1 + i, s2);
 	return (s1);
 }
