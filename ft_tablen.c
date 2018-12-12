286f1141e6376757e09c88614e666cea4e3d56e4
bpisano <bpisano@z4r7p5.le-101.fr>
2018-12-12 22:01:23 +0100

    norme

--- ft_tablen.c
+++ ft_tablen.c
@@ -3,19 +3,19 @@
 /*                                                              /             */
 /*   ft_tablen.c                                      .::    .:/ .      .::   */
 /*                                                 +:+:+   +:    +:  +:+:+    */
-/*   By: bpisano <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
+/*   By: bpisano <bpisano@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/10/04 19:06:25 by bpisano      #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/04 19:06:34 by bpisano     ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/12/12 21:59:52 by bpisano     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t      ft_tablen(char **tab)
+size_t	ft_tablen(char **tab)
 {
-	int     i;
+	int		i;
 
 	i = -1;
 	while (tab[++i])
