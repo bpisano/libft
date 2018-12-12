3986ae451606f9337b6be1326ccf503097486e6d
bpisano <bpisano@z4r7p5.le-101.fr>
2018-12-12 23:21:48 +0100

    norme

--- ft_mask.c
+++ ft_mask.c
@@ -3,23 +3,23 @@
 /*                                                              /             */
 /*   ft_mask.c                                        .::    .:/ .      .::   */
 /*                                                 +:+:+   +:    +:  +:+:+    */
-/*   By: anamsell <anamsell@student.le-101.fr>      +:+   +:    +:    +:+     */
+/*   By: bpisano <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
-/*   Created: 2018/10/15 11:14:34 by bpisano      #+#   ##    ##    #+#       */
-/*   Updated: 2018/12/12 14:32:33 by bpisano     ###    #+. /#+    ###.fr     */
+/*   Created: 2018/12/12 23:20:25 by bpisano      #+#   ##    ##    #+#       */
+/*   Updated: 2018/12/12 23:20:41 by bpisano     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-long    ft_mask(int c)
+long	ft_mask(int c)
 {
-    long    l;
+	long	l;
 
-    l = c & 0xff;
-    l |= (l << 8);
-    l |= (l << 16);
-    l |= (l << 16) << 16;
-    return (l);
+	l = c & 0xff;
+	l |= (l << 8);
+	l |= (l << 16);
+	l |= (l << 16) << 16;
+	return (l);
 }
